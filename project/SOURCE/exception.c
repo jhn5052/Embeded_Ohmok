e2ea323c10bc574a1752950550474e8e3e9772a6
jhn5052 <jhn5052@naver.com>
2020-05-21 17:59:50 +0900

    Embeded Project

---
+++ project/SOURCE/exception.c
@@ -0,0 +1,61 @@
+#include "2450addr.h"
+#include "macro.h"
+
+void Exception_Init(void);
+void Undef_Handler(void);
+void Pabort_Handler(void);
+void Dabort_Handler(void);
+void SWI_Handler(void);
+
+extern unsigned int  HandleUndef;
+extern unsigned int  HandleSWI;
+extern unsigned int  HandlePabort;
+extern unsigned int  HandleDabort;
+
+void Uart_Printf(const char *fmt,...);
+
+void SWI_Handler(void)
+{
+	Uart_Printf("SWI exception.\n");
+	for(;;);
+}
+
+void Undef_Handler(void) 
+{
+	Uart_Printf("Undefined instruction exception.\n");
+	for(;;);
+}
+
+void Pabort_Handler(void)
+{
+	volatile int prefetch_addr=0;
+	
+	Uart_Printf("Pabort exception in address 0x%08x.\n", prefetch_addr);		
+	for(;;);
+}
+
+void Dabort_Handler(void)
+{
+	volatile int data_addr=0;
+
+	Uart_Printf("Dabort exception in address 0x%08x.\n", data_addr);				
+	for(;;);
+}
+
+void Exception_Init(void)
+{
+	HandleUndef  = (unsigned)Undef_Handler;
+	HandleSWI    = (unsigned)SWI_Handler;
+	HandlePabort = (unsigned)Pabort_Handler;
+	HandleDabort = (unsigned)Dabort_Handler;
+	// Disable Interrupt
+	rINTMOD1   = 0x0;
+	rINTMSK1    = BIT_ALLMSK;		
+	rINTMOD2   = 0x0;
+	rINTMSK2    = BIT_ALLMSK;		
+
+	// Clear All Previous Pending Bits
+	rSRCPND1 = 0xffffffff;
+	rSRCPND2 = 0xffffffff;
+	rINTPND1 = 0xffffffff;  
+}
---
+++ project/SOURCE/touch.c
@@ -0,0 +1,15 @@
+#include "2450addr.h"
+
+void Touch_Init(void)
+{
+	rADCDLY = (50000); 
+     /* TO DO : prescaler enable, prescaler value=39, Analog no input, 
+      * 		Normal operation mode, Disable start, No operation */
+	rADCCON =(1<<14)|(39<<6)|(0<<3)|(0<<2)|(0<<1)|(0);  
+	 
+	 /* TO DO :  For Waiting Interrupt Mode rADCTSC=0xd3 */
+	rADCTSC =(0<<8)|(1<<7)|(1<<6)|(1<<4)|(0<<3)|(0<<2)|(3);
+
+	
+}
+
